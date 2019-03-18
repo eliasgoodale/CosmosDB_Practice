95f85a6d1214bbacc55a342013154ef19e793d83
42 Robotics <42robotics@42s-iMac-5.local>
2019-03-18 16:49:24 -0700

    saving

---
+++ QueryTest/QueryTest/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Azure.Documents.Client;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Cosmonaut;
+using Cosmonaut.Attributes;
+using Microsoft.AspNetCore.Http.Features;
+using Newtonsoft.Json;
+
+namespace QueryTest
+{
+    [CosmosCollection("indexTagAdminItems")]
+    public class Index : ISharedCosmosEntity {
+        public string CosmosEntityName { get; set; }
+        
+        [JsonProperty("id")]
+        public string Id { get; set; }
+
+        public string Name { get; set; }
+        
+        public string Location { get; set; }
+
+        public bool Enabled { get; set; }
+    }
+    
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            const string EndpointUrl = @"";
+            const string PrimaryKey =
+                @"";
+            DocumentClient client;
+            client = new DocumentClient(new Uri(EndpointUrl), PrimaryKey);
+            var queryOptions = new FeedOptions { MaxItemCount = -1 };
+
+            IQueryable<Index> familyQuery = client.CreateDocumentQuery<Index>(
+                UriFactory.CreateDocumentCollectionUri("index_tag_admin_db", "indexTagAdminItems"));
+            foreach (var index in familyQuery)
+            {
+                Console.WriteLine("\tRead {0}", index.Id + " " + index.Name);
+            }
+
+            Console.Read();
+
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>();
+    }
+}
